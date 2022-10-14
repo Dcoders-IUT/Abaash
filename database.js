7eeb9e5e6b9a96958a1bfad2592dc18b6d1f01ac
rafio-iut <irfanurrahman5@iut-dhaka.edu>
2022-10-14 18:42:43 +0600

    local variable

--- database.js
+++ database.js
@@ -2,41 +2,25 @@ const mariadb = require('mariadb');
 
 // const database = {};
 
-// async
-const exec = (command) => {
-    // let db;
-    // try {
-    //     db = await mariadb.createConnection({
-    //         host: 'localhost',
-    //         user: 'user1',
-    //         password: 'password1',
-    //         database: 'abaash',
-    //     });
+const conn = mariadb.createConnection({
+    host: 'localhost',
+    user: 'user1',
+    password: 'password1',
+    database: 'abaash',
+});
 
-    //     db.query(command);
-    // } catch (err) {
-    //     console.log(`SQL Connection Error!\n${err}`);
-    // } finally {
-    //     if (db) db.close();
-    // }
+async function exec(command) {
+    await conn.then((db) => db.query(command));
+}
 
-    mariadb
-        .createConnection({
-            host: 'localhost',
-            user: 'user1',
-            password: 'password1',
-            database: 'abaash',
-        })
-        .then((db) => {
-            db.query(command);
-        });
+async function get(command) {
+    await conn;
+    .then((db) => db.query(command));
+}
+
+const get = (command) => {
+    conn.then((db) => {
+        db.query(command);
+    });
 };
-
-const newplc = () => {
-    const temp = new Date();
-    const str = `${temp.getUTCFullYear()}-${temp.getUTCMonth()}-${temp.getUTCDate()}T${temp.getUTCHours()}:${temp.getUTCMinutes()}:${temp.getUTCSeconds()}.${temp.getUTCMilliseconds()}`;
-
-    return str;
-};
-
-module.exports = { exec, newplc };
+module.exports = { exec, get };
