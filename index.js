848bbca51754a40d53866d6b745a6bfeb369475c
orkhasnat <orkhasnat@gmail.com>
2022-10-23 03:53:43 +0600

    some refactoring

--- index.js
+++ index.js
@@ -1,107 +1,107 @@
-const express = require('express');
-const store = require('store');
-const database = require('./util/database');
-const misc = require('./util/misc');
+const express = require('express')
+const store = require('store')
+const database = require('./backend/util/database')
+const misc = require('./backend/util/misc')
+const path = require("path")
+const app = express()
+const port = 3001
 
-const app = express();
-const port = 3001;
-
-app.use(express.static('public'));
-app.set('view engine', 'ejs');
+app.use(express.static('frontend'))
+app.set('views', path.join(__dirname, './backend/views'))
+app.set('view engine', 'ejs')
 
 async function allFlats() {
     try {
-        return await database.get('SELECT flatID, name, address, gender, level FROM flat');
+        return await database.get('SELECT flatID, name, address, gender, level FROM flat')
     } catch (err) {
-        return {};
+        return {}
     }
 }
 
 function openEJS(page, res) {
-    const currentUser = store.get('user');
-    const mode = store.get('mode');
+    const currentUser = store.get('user')
+    const mode = store.get('mode')
 
-    res.render(page, { currentUser, mode });
+    res.render(page, { currentUser, mode })
 }
 
 async function openHomeEJS(res) {
-    const currentUser = store.get('user');
-    const mode = store.get('mode');
+    const currentUser = store.get('user')
+    const mode = store.get('mode')
 
     if (!store.get('mode') || !store.get('user')) {
         res.render('home', {
             currentUser,
             mode,
             flatList: misc.shuffle(await allFlats()),
-        });
-        return;
+        })
+        return
     }
-    let currentUserData;
+    let currentUserData
 
     try {
         currentUserData = await database.getUnique(
-            `SELECT name FROM ${mode} WHERE ${
-                mode === 'student' ? 'studentID' : 'username'
+            `SELECT name FROM ${mode} WHERE ${mode === 'student' ? 'studentID' : 'username'
             }='${currentUser}'`,
-        );
+        )
     } catch (err) {
         res.render('home', {
             currentUser,
             mode,
             nameOfCurrentUser: null,
             flatList: misc.shuffle(await allFlats()),
-        });
-        return;
+        })
+        return
     }
     res.render('home', {
         currentUser,
         mode,
         nameOfCurrentUser: currentUserData.name,
         flatList: misc.shuffle(await allFlats()),
-    });
+    })
 }
 
 app.route('/')
     .get(async (req, res) => openHomeEJS(res))
-    .post(async (req, res) => openHomeEJS(res));
+    .post(async (req, res) => openHomeEJS(res))
 
 app.get('/map', (req, res) => {
-    if (!store.get('mode') || !store.get('user')) res.redirect('./');
-    else openEJS('map', res);
-});
+    if (!store.get('mode') || !store.get('user')) res.redirect('./')
+    else openEJS('map', res)
+})
 
 app.get('/notifications', (req, res) => {
-    if (!store.get('mode') || !store.get('user')) res.redirect('./');
-    else openEJS('notifications', res);
-});
+    if (!store.get('mode') || !store.get('user')) res.redirect('./')
+    else openEJS('notifications', res)
+})
 
 app.get('/sos', (req, res) => {
-    if (!store.get('mode') || !store.get('user')) res.redirect('./');
-    else openEJS('sos', res);
-});
+    if (!store.get('mode') || !store.get('user')) res.redirect('./')
+    else openEJS('sos', res)
+})
 
 app.get('/logout', (req, res) => {
-    store.remove('user');
-    store.remove('mode');
-    res.redirect('./');
-});
+    store.remove('user')
+    store.remove('mode')
+    res.redirect('./')
+})
 
 app.get('/profile', (req, res) => {
-    if (!store.get('mode') || !store.get('user')) res.redirect('./');
+    if (!store.get('mode') || !store.get('user')) res.redirect('./')
     else {
-        const currentUser = store.get('user');
-        const mode = store.get('mode');
+        const currentUser = store.get('user')
+        const mode = store.get('mode')
 
-        res.redirect(`${mode}/profile/${currentUser}`);
+        res.redirect(`${mode}/profile/${currentUser}`)
     }
-});
+})
 
-const studentRouter = require('./routes/student');
-const ownerRouter = require('./routes/owner');
-const flatRouter = require('./routes/flat');
+const studentRouter = require('./backend/routes/student')
+const ownerRouter = require('./backend/routes/owner')
+const flatRouter = require('./backend/routes/flat')
 
-app.use('/student', studentRouter);
-app.use('/owner', ownerRouter);
-app.use('/flat', flatRouter);
+app.use('/student', studentRouter)
+app.use('/owner', ownerRouter)
+app.use('/flat', flatRouter)
 
-app.listen(port);
+app.listen(port)
