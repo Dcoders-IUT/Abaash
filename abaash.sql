eff92879c9aa30b33ccd57796bb3bdfb9c28e3a6
rafio-iut <irfanurrahman5@iut-dhaka.edu>
2022-11-19 22:59:02 +0600

    DATABASE

--- abaash.sql
+++ abaash.sql
@@ -34,24 +34,25 @@ CREATE TABLE IF NOT EXISTS `flat` (
   `lift` tinyint(1) NOT NULL,
   `generator` tinyint(1) NOT NULL,
   `rent` int(10) DEFAULT NULL,
+  `message` text DEFAULT NULL,
   `photo` varchar(200) DEFAULT NULL,
   PRIMARY KEY (`flatID`) USING BTREE,
   KEY `flat_ibfk_1` (`owner`),
   CONSTRAINT `flat_ibfk_1` FOREIGN KEY (`owner`) REFERENCES `owner` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table abaash.flat: ~0 rows (approximately)
+-- Dumping data for table abaash.flat: ~8 rows (approximately)
 DELETE FROM `flat`;
 /*!40000 ALTER TABLE `flat` DISABLE KEYS */;
-INSERT INTO `flat` (`flatID`, `name`, `address`, `description`, `owner`, `gender`, `x`, `y`, `level`, `area`, `lift`, `generator`, `rent`, `photo`) VALUES
-	(1001734, 'Sanctuary', 'Kolemeshor', '', 'ork', 1, 23.94897163115608, 90.3798185760795, 2, 800, 0, 1, 3000, NULL),
-	(1002726, 'Sanctuary', 'Kolemeshor', '', 'ork', 1, 23.94897163115608, 90.3798185760795, 1, 800, 1, 0, 3000, NULL),
-	(1003432, 'Sanctuary', 'Kolemeshor', '', 'ork', 1, 23.94897163115608, 90.3798185760795, 2, 800, 0, 1, 3000, NULL),
-	(1004979, 'Glorious Housing', 'Kathora', '', 'ork', 0, 23.94897163115608, 90.3798185760795, 3, 1350, 0, 1, 8000, NULL),
-	(1129304, 'Sattar House', 'Opposite of IUT', '', 'ork', 0, 23.9489922, 90.3797594, 6, 1700, 0, 0, 4000, NULL),
-	(1158151, 'New Home', 'Board Bazar', '', 'arifin', 0, 23.819028104444815, 90.45262759742008, 8, 850, 1, 1, 3500, NULL),
-  (1158152, 'OLD Home', 'Board Bazar', '', 'arifin', 1, 23.819028104444816, 90.45262759742009, 10, 1000, 1, 1, 5000, NULL),
-	(1438758, 'MAA Tower', 'Kathora', '', 'ork', 0, 23.94897163115608, 90.3798185760795, 19, 1500, 1, 1, 7000, NULL);
+INSERT INTO `flat` (`flatID`, `name`, `address`, `description`, `owner`, `gender`, `x`, `y`, `level`, `area`, `lift`, `generator`, `rent`, `message`, `photo`) VALUES
+	(1001734, 'Sanctuary', 'Kolemeshor', '/', 'ork', 1, 23.94897163115608, 90.3798185760795, 2, 800, 0, 1, 3000, 'Hello, My name is Ork.', NULL),
+	(1002726, 'Sanctuary', 'Kolemeshor', '', 'ork', 1, 23.94897163115608, 90.3798185760795, 1, 800, 1, 0, 3000, NULL, NULL),
+	(1003432, 'Sanctuary', 'Kolemeshor', '', 'ork', 1, 23.94897163115608, 90.3798185760795, 2, 800, 0, 1, 3000, NULL, NULL),
+	(1004979, 'Glorious Housing', 'Kathora', '', 'ork', 0, 23.94897163115608, 90.3798185760795, 3, 1350, 0, 1, 8000, NULL, NULL),
+	(1129304, 'Sattar House', 'Opposite of IUT', '', 'ork', 0, 23.9489922, 90.3797594, 6, 1700, 0, 0, 4000, NULL, NULL),
+	(1158151, 'New Home', 'Board Bazar', '', 'arifin', 0, 23.819028104444815, 90.45262759742008, 8, 850, 1, 1, 3500, NULL, NULL),
+	(1158152, 'OLD Home', 'Board Bazar', '', 'arifin', 1, 23.819028104444815, 90.45262759742009, 10, 1000, 1, 1, 5000, NULL, NULL),
+	(1438758, 'MAA Tower', 'Kathora', '', 'ork', 0, 23.94897163115608, 90.3798185760795, 19, 1500, 1, 1, 7000, NULL, NULL);
 /*!40000 ALTER TABLE `flat` ENABLE KEYS */;
 
 -- Dumping structure for table abaash.flatrequest
@@ -60,6 +61,7 @@ CREATE TABLE IF NOT EXISTS `flatrequest` (
   `studentID` int(9) NOT NULL,
   `flatID` int(12) NOT NULL,
   `date` varchar(100) NOT NULL,
+  `message` text DEFAULT NULL,
   PRIMARY KEY (`studentID`,`flatID`),
   KEY `flatrequest_ibfk_2` (`flatID`),
   CONSTRAINT `flatrequest_ibfk_1` FOREIGN KEY (`studentID`) REFERENCES `student` (`studentID`) ON DELETE CASCADE ON UPDATE CASCADE,
@@ -69,9 +71,8 @@ CREATE TABLE IF NOT EXISTS `flatrequest` (
 -- Dumping data for table abaash.flatrequest: ~2 rows (approximately)
 DELETE FROM `flatrequest`;
 /*!40000 ALTER TABLE `flatrequest` DISABLE KEYS */;
-INSERT INTO `flatrequest` (`studentID`, `flatID`, `date`) VALUES
-	(112, 1003432, '2022-9-28T8:56:35.97'),
-	(190041120, 1002726, '2022-9-28T8:10:22.870');
+INSERT INTO `flatrequest` (`studentID`, `flatID`, `date`, `message`) VALUES
+	(129, 1001734, '2022-10-19T16:50:50.881', 'Hello, My name is Jubayer.');
 /*!40000 ALTER TABLE `flatrequest` ENABLE KEYS */;
 
 -- Dumping structure for table abaash.owner
@@ -91,14 +92,14 @@ CREATE TABLE IF NOT EXISTS `owner` (
   UNIQUE KEY `nid` (`nid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table abaash.owner: ~3 rows (approximately)
+-- Dumping data for table abaash.owner: ~4 rows (approximately)
 DELETE FROM `owner`;
 /*!40000 ALTER TABLE `owner` DISABLE KEYS */;
 INSERT INTO `owner` (`name`, `username`, `password`, `passwordLastChanged`, `phone`, `email`, `nid`, `photo`) VALUES
 	('Sherajul Arifin', 'arifin', '65ca3d04be0cc68c1a7003f8aab5e7c9f93d83fa94351f91925e4dcabb5065fe', '2022-9-25T14:13:17.638', 1731568888, 'gmail@arifin.com', 65274196, NULL),
-  ('Sumit Alam Khan', 'sumit', 'cb19b4edd90562692900e12c8c19f7e1b3240b88831e46e8fffee6cc88fc0664', '2022-10-18T20:57:36.383', 1731569827, 'sumit@iut.edu', 85274196, NULL),
-	('Ork the Bariola', 'ork', 'f6c3a82f8d5ed8b78bfd15d639410719ce24d8393fb020a722c298831d8a6555', '2022-9-15T3:29:21.858', 1731969827, 'ork@bariola.com', 2000785, 'ork1.png'),
-	('Tanvir Hasan Saikat', 'saikat', 'c3f604b8a8b797a0d402738c7f588511555702ef8e73bd78dfd37a9ef3c50877', '2022-10-12T13:6:8.284', 1745986461, 'saikat@gmail.com', 46541646, NULL);
+	('Ork the Bariola', 'ork', 'f6c3a82f8d5ed8b78bfd15d639410719ce24d8393fb020a722c298831d8a6555', '2022-9-15T3:29:21.858', 1731969827, 'ork@bariola.com', 2000785, 'ork1668854173872.png'),
+	('Tanvir Hasan Saikat', 'saikat', 'c3f604b8a8b797a0d402738c7f588511555702ef8e73bd78dfd37a9ef3c50877', '2022-10-12T13:6:8.284', 1745986461, 'saikat@gmail.com', 46541646, NULL),
+	('Sumit Alam Khan', 'sumit', 'cb19b4edd90562692900e12c8c19f7e1b3240b88831e46e8fffee6cc88fc0664', '2022-10-18T20:57:36.383', 1731569827, 'sumit@iut.edu', 85274196, NULL);
 /*!40000 ALTER TABLE `owner` ENABLE KEYS */;
 
 -- Dumping structure for table abaash.room
@@ -148,7 +149,7 @@ CREATE TABLE IF NOT EXISTS `student` (
   UNIQUE KEY `nid` (`nid`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table abaash.student: ~4 rows (approximately)
+-- Dumping data for table abaash.student: ~3 rows (approximately)
 DELETE FROM `student`;
 /*!40000 ALTER TABLE `student` DISABLE KEYS */;
 INSERT INTO `student` (`name`, `gender`, `studentID`, `password`, `passwordLastChanged`, `phone`, `email`, `nid`, `bloodgroup`, `photo`) VALUES
