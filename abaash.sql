200dd037f176a106efc5c42f1786738f9f8b036f
rafio-iut <irfanurrahman5@iut-dhaka.edu>
2022-11-25 13:17:48 +0600

    Database

--- abaash.sql
+++ abaash.sql
@@ -41,7 +41,7 @@ CREATE TABLE IF NOT EXISTS `flat` (
   CONSTRAINT `flat_ibfk_1` FOREIGN KEY (`owner`) REFERENCES `owner` (`username`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table abaash.flat: ~7 rows (approximately)
+-- Dumping data for table abaash.flat: ~8 rows (approximately)
 DELETE FROM `flat`;
 /*!40000 ALTER TABLE `flat` DISABLE KEYS */;
 INSERT INTO `flat` (`flatID`, `name`, `address`, `description`, `owner`, `gender`, `x`, `y`, `level`, `area`, `lift`, `generator`, `rent`, `message`, `photo`) VALUES
@@ -50,6 +50,7 @@ INSERT INTO `flat` (`flatID`, `name`, `address`, `description`, `owner`, `gender
 	(1004979, 'Glorious Housing', 'Kathora', 'Best female residence outside IUT.', 'ork', 0, 23.94897163115608, 90.3798185760795, 3, 1350, 0, 1, 8000, 'Please include your hometown in your message.', NULL),
 	(1129304, 'Sattar House', 'Opposite of IUT', 'You can wake up at 7:55 and reach class on time!', 'ork', 0, 23.9489922, 90.3797594, 6, 1700, 0, 0, 4000, 'Please include your hometown in your message.', NULL),
 	(1158151, 'New Home', 'Board Bazar', 'Just beside the mosque.', 'arifin', 0, 23.94462222232767, 90.38188038666934, 8, 850, 1, 1, 3500, 'Please include your hometown in your message.', NULL),
+	(1276633, '', '', '', 'arifin', 1, 0, 0, 1, 0, 0, 0, 0, '', NULL),
 	(1438758, 'MAA Tower', 'Kathora', 'You wont feel your mother\'s absense!', 'ork', 0, 23.94897163115608, 90.3798185760795, 19, 1500, 1, 1, 7000, 'Please include your hometown in your message.', NULL),
 	(1556134, 'Housing Complex', 'Bashundhara', 'Best', 'arifin', 1, 23.815276, 90.448183, 20, 22000, 0, 0, 85000, 'You must have CGPA 3.9 or above to rent this flat.', 'arifin1669012891745.jpg');
 /*!40000 ALTER TABLE `flat` ENABLE KEYS */;
@@ -67,9 +68,11 @@ CREATE TABLE IF NOT EXISTS `flatrequest` (
   CONSTRAINT `flatrequest_ibfk_2` FOREIGN KEY (`flatID`) REFERENCES `flat` (`flatID`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table abaash.flatrequest: ~3 rows (approximately)
+-- Dumping data for table abaash.flatrequest: ~1 rows (approximately)
 DELETE FROM `flatrequest`;
 /*!40000 ALTER TABLE `flatrequest` DISABLE KEYS */;
+INSERT INTO `flatrequest` (`studentID`, `flatID`, `date`, `message`) VALUES
+	(190041129, 1556134, '2022-11-25T4:35:0.296', 'Parle theke dekhao');
 /*!40000 ALTER TABLE `flatrequest` ENABLE KEYS */;
 
 -- Dumping structure for table abaash.owner
@@ -123,6 +126,7 @@ INSERT INTO `room` (`flatid`, `bed`, `din`, `liv`, `kit`, `bath`, `balk`, `xtra`
 	(1004979, 0, 0, 0, 0, 0, 0, 0),
 	(1129304, 3, 1, 1, 1, 2, 2, 0),
 	(1158151, 0, 0, 0, 0, 0, 0, 0),
+	(1276633, 0, 0, 0, 0, 0, 0, 0),
 	(1438758, 0, 0, 0, 0, 0, 0, 0),
 	(1556134, 5, 1, 2, 1, 4, 3, 0);
 /*!40000 ALTER TABLE `room` ENABLE KEYS */;
@@ -149,10 +153,10 @@ CREATE TABLE IF NOT EXISTS `student` (
 DELETE FROM `student`;
 /*!40000 ALTER TABLE `student` DISABLE KEYS */;
 INSERT INTO `student` (`name`, `gender`, `studentID`, `password`, `passwordLastChanged`, `phone`, `email`, `nid`, `bloodgroup`, `photo`) VALUES
-	('Fardin', 1, 190041112, '519211b1c823753d0db208af050b9fe855b7379aeacf1f7e4b834c5e7378390e', '2022-9-28T7:15:16.53', 1930305520, 'mahtabnur@iut-dhaka.edu', 0, 'AB+', NULL),
+	('Fardin', 1, 190041112, '519211b1c823753d0db208af050b9fe855b7379aeacf1f7e4b834c5e7378390e', '2022-9-28T7:15:16.53', 1930305520, 'mahtabnur@iut-dhaka.edu', 0, 'AB+', '1669298882073.jpg'),
 	('Reaz Hassan Joarder', 1, 190041117, '5c647824cbd72e2f7108a0e39f48d5540309f05ded4f6fabe0005797ac4c24ea', '2022-10-20T18:10:53.467', 1986731598, 'reazhassan@iut-dhaka.edu', 93461935, 'A+', NULL),
 	('Nafi', 1, 190041120, 'ff20e2239111482880e23eb82ddce6e3ad3ea4ec9914fa231181a76ff220a3da', '2022-9-28T6:50:41.680', 1498714970, 'nafi120@gmail.com', 123164867, 'B+', NULL),
-	('Jubayer', 1, 190041129, 'bc8e7191c2fae4af85e6b728bcec435a48fe18faf0ee58d710800d0da33be456', '2022-9-14T16:12:16.155', 1829977462, 'jubayerislam@iut-dhaka.edu', 0, 'A+', NULL);
+	('Jubayer', 1, 190041129, 'bc8e7191c2fae4af85e6b728bcec435a48fe18faf0ee58d710800d0da33be456', '2022-9-14T16:12:16.155', 1829977462, 'jubayerislam@iut-dhaka.edu', 0, 'A+', '1669196635439.jpg');
 /*!40000 ALTER TABLE `student` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
